a1096fff4142401b843f692b739b7816eec9d60d
AslinMartinez16 <110441889+AslinMartinez16@users.noreply.github.com>
2022-08-14 03:19:21 -0600

    Login
    Cambios en el Login

---
+++ ProyectoLenguajeI/ProyectoLenguajeI/Login.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace ProyectoLenguajeI
+{
+    partial class InicioFormulario
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InicioFormulario));
+            this.label1 = new System.Windows.Forms.Label();
+            this.UsuarioTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.CancelarButton = new System.Windows.Forms.Button();
+            this.AceptarButton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario:";
+            // 
+            // UsuarioTextBox
+            // 
+            this.UsuarioTextBox.Location = new System.Drawing.Point(132, 24);
+            this.UsuarioTextBox.Name = "UsuarioTextBox";
+            this.UsuarioTextBox.Size = new System.Drawing.Size(290, 24);
+            this.UsuarioTextBox.TabIndex = 1;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(132, 82);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.PasswordChar = '*';
+            this.PasswordTextBox.Size = new System.Drawing.Size(290, 24);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Contraseña:";
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CancelarButton.Image = global::ProyectoLenguajeI.Properties.Resources.boton_eliminar;
+            this.CancelarButton.Location = new System.Drawing.Point(280, 125);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(142, 43);
+            this.CancelarButton.TabIndex = 5;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.CancelarButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.CancelarButton.UseVisualStyleBackColor = true;
+            // 
+            // AceptarButton
+            // 
+            this.AceptarButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AceptarButton.Image = global::ProyectoLenguajeI.Properties.Resources.aceptar;
+            this.AceptarButton.Location = new System.Drawing.Point(132, 125);
+            this.AceptarButton.Name = "AceptarButton";
+            this.AceptarButton.Size = new System.Drawing.Size(142, 43);
+            this.AceptarButton.TabIndex = 4;
+            this.AceptarButton.Text = "Aceptar";
+            this.AceptarButton.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.AceptarButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.AceptarButton.UseVisualStyleBackColor = true;
+            this.AceptarButton.Click += new System.EventHandler(this.AceptarButton_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // InicioFormulario
+            // 
+            this.AcceptButton = this.AceptarButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.CancelarButton;
+            this.ClientSize = new System.Drawing.Size(461, 189);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.AceptarButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UsuarioTextBox);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "InicioFormulario";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio de Sesión";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox UsuarioTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button AceptarButton;
+        private System.Windows.Forms.Button CancelarButton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ ProyectoLenguajeI/ProyectoLenguajeI/Login.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+
+namespace ProyectoLenguajeI
+{
+    public partial class InicioFormulario : Form
+    {
+        public InicioFormulario()
+        {
+            InitializeComponent();
+        }
+
+        Usuario user;
+        string _nombreUsuario = "DEUNA";
+        string _password = "1234";
+
+        private void AceptarButton_Click(object sender, EventArgs e)
+        {
+            if ( UsuarioTextBox.Text == string.Empty)
+            {
+                errorProvider1.SetError(UsuarioTextBox, "Ingrese el nombre de usuario.");
+                UsuarioTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+
+
+            if (PasswordTextBox.Text == string.Empty)
+            {
+                errorProvider1.SetError(PasswordTextBox, "Ingrese la contraseña");
+                PasswordTextBox.Focus();
+                return;
+            }
+            errorProvider1.Clear();
+            user = new Usuario(UsuarioTextBox.Text, PasswordTextBox.Text);
+            
+            if (user.Codigo.ToUpper() == _nombreUsuario && user.Clave == _password)
+            {
+                Menu miMenu = new Menu();
+                this.Hide();
+                miMenu.Show();
+            }
+            else
+            {
+                MessageBox.Show("Datos de usuario incorrectos", "Alerta", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
